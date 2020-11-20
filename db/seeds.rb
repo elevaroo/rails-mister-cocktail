a20590c77bac011b129b542f07facf191b55ca10
Eleonore van Roosendaal <eleonore.roosendaal@hotmail.com>
2020-11-20 12:35:20 +0100

    Add dotenv - Protect my secret data in .env file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@ require 'json'
 require 'open-uri'
 
 Ingredient.destroy_all
+Cocktail.destroy_all
 puts 'database is clean'
 
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
@@ -11,3 +12,9 @@ user = JSON.parse(user_serialized)
 user['drinks'].each do |ingredient|
   p Ingredient.create!(name: ingredient['strIngredient1'])
 end
+
+mojito = Cocktail.create!(name: 'Mojito')
+espresso_martini = Cocktail.create!(name: 'Espresso Martini')
+kentucky_buck = Cocktail.create!(name: 'Kentucky Buck')
+basil_smash = Cocktail.create!(name: 'Basil Smash')
+
